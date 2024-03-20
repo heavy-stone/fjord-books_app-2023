b4de0bd2ff346811dd641f6b482b6802f5266763
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-20 20:37:39 +0900

    add user index, show pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'books#index'
+  resources :users, only: %i[index show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
