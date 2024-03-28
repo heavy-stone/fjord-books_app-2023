ced7e3fc7cef298ad4a953851c437cd650027c8b
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-28 15:07:50 +0900

    update report.user_id to bigint from integer

--- db/migrate/20240327110430_create_reports.rb
+++ db/migrate/20240327110430_create_reports.rb
@@ -1,7 +1,7 @@
 class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, type: :bigint
       t.string :title
       t.text :content
 
