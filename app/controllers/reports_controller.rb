ed619a13663e300e4828015eb8692cd8d5c23258
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-29 20:34:37 +0900

    make it possible to see other user reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[show edit update destroy]
+  before_action :set_current_user_report, only: %i[edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    @reports = current_user_reports.page(params[:page])
+    @reports = Report.order(id: :desc).page(params[:page])
   end
 
-  def show; end
+  def show
+    @report = Report.find(params[:id])
+  end
 
   def new
     @report = Report.new
@@ -18,11 +20,12 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = Report.new(report_params.merge(user_id: current_user.id))
+    @report = Report.new(report_params.merge(user: current_user))
 
     if @report.save
       redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
+      flash.now[:alert] = t('controllers.common.alert_create', name: Report.model_name.human)
       render :new, status: :unprocessable_entity
     end
   end
@@ -31,25 +34,24 @@ class ReportsController < ApplicationController
     if @report.update(report_params)
       redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
+      flash.now[:alert] = t('controllers.common.alert_update', name: Report.model_name.human)
       render :edit, status: :unprocessable_entity
-
     end
   end
 
   def destroy
-    @report.destroy
-
-    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+    if @report.destroy
+      redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
+    else
+      flash.now[:alert] = t('controllers.common.alert_destroy', name: Report.model_name.human)
+      render :show, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def current_user_reports
-    Report.where(user_id: current_user.id).order(id: :desc)
-  end
-
-  def set_report
-    @report = current_user_reports.find(params[:id])
+  def set_current_user_report
+    @report = current_user.reports.find(params[:id])
   end
 
   def report_params
