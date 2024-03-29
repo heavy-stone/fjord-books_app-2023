61df2c375e3c44421ef29f00e13dfccaf8168a73
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-29 15:41:53 +0900

    add report function

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,70 +1,62 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
 
-  # GET /reports or /reports.json
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
-    @reports = Report.all
+    @reports = current_user_reports.page(params[:page])
   end
 
-  # GET /reports/1 or /reports/1.json
-  def show
-  end
+  def show; end
 
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
+    @report = Report.new(report_params.merge(user_id: current_user.id))
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
 
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.require(:report).permit(:user_id, :title, :content)
-    end
+  def current_user_reports
+    Report.where(user_id: current_user.id).order(id: :desc)
+  end
+
+  def set_report
+    @report = current_user_reports.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:user_id, :title, :content)
+  end
+
+  def record_not_found
+    redirect_to reports_url, alert: t('activerecord.errors.messages.not_found', record: Report.model_name.human)
+  end
 end
