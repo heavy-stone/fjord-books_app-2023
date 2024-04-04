1afa3262ecb6d0e4bd33a40b3c066cb0da6ed506
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-04 16:55:44 +0900

    add auto link to url string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,8 @@ module ApplicationHelper
   end
 
   def format_content(content)
-    safe_join(content.split("\n"), tag.br)
+    safe_joined_content = safe_join(content.split("\n"), tag.br)
+    uri_reg = URI::DEFAULT_PARSER.make_regexp(%w[http https])
+    safe_joined_content.gsub(uri_reg) { %(<a href='#{::Regexp.last_match(0)}' target='_blank'>#{::Regexp.last_match(0)}</a>) }
   end
 end
