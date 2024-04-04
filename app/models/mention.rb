6fbd170b35cb7a85975ae5abd0d33a40ae12d045
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-04 15:28:27 +0900

    add relationship between report and mention

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Mention < ApplicationRecord
+  belongs_to :source, class_name: 'Report', inverse_of: :mentioned_sources
+  belongs_to :destination, class_name: 'Report', inverse_of: :mentioning_destinations
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,16 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :mentioned_sources, class_name: 'Mention',
+                               foreign_key: :source_id,
+                               inverse_of: :source,
+                               dependent: :destroy
+  has_many :mentioned_reports, through: :mentioned_sources, source: :destination
+  has_many :mentioning_destinations, class_name: 'Mention',
+                                     foreign_key: :destination_id,
+                                     inverse_of: :destination,
+                                     dependent: :destroy
+  has_many :mentioning_reports, through: :mentioning_destinations, source: :source
 
   validates :title, presence: true
   validates :content, presence: true
