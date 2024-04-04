f8e40e9f0b205bcb82e2a7cf00b5720c7750ac43
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-04 14:48:16 +0900

    add mentions table

---
+++ db/migrate/20240402075355_create_mentions.rb
@@ -0,0 +1,12 @@
+class CreateMentions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mentions do |t|
+      t.integer :source_id, null: false, index: true
+      t.integer :destination_id, null: false, index: true
+
+      t.timestamps
+    end
+    add_foreign_key :mentions, :reports, column: :source_id
+    add_foreign_key :mentions, :reports, column: :destination_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_075355) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "mentions", force: :cascade do |t|
+    t.integer "source_id", null: false
+    t.integer "destination_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["destination_id"], name: "index_mentions_on_destination_id"
+    t.index ["source_id"], name: "index_mentions_on_source_id"
+  end
+
   create_table "reports", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -87,5 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_082433) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "mentions", "reports", column: "destination_id"
+  add_foreign_key "mentions", "reports", column: "source_id"
   add_foreign_key "reports", "users"
 end
