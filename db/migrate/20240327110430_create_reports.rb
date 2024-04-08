b58d62564f897437d7c21d36e6739a97635e5e51
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-08 15:04:47 +0900

    remove type: :bigint

--- db/migrate/20240327110430_create_reports.rb
+++ db/migrate/20240327110430_create_reports.rb
@@ -1,7 +1,7 @@
 class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
-      t.references :user, null: false, foreign_key: true, type: :bigint
+      t.references :user, null: false, foreign_key: true
       t.string :title
       t.text :content
 
