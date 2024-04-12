0b97db54d1d9a039714744e505f14052717746ef
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-12 15:13:53 +0900

    add model tests

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be editable' do
+    user = User.new
+    report = user.reports.new
+    assert report.editable?(user)
+  end
+
+  test 'should not be editable' do
+    user1 = User.new
+    user2 = User.new
+    report = user1.reports.new
+    assert_not report.editable?(user2)
+  end
+
+  test 'should get created on' do
+    now = Time.current
+    report = Report.new(created_at: now)
+    assert_equal now.to_date, report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get name' do
+    user = User.new(name: 'John Doe', email: 'foo@example.com')
+    assert_equal 'John Doe', user.name_or_email
+  end
+
+  test 'should get email' do
+    user = User.new(name: '', email: 'foo@example.com')
+    assert_equal 'foo@example.com', user.name_or_email
+  end
 end
