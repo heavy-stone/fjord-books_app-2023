bca91ed87a95a662de90093860d59ec5eb84cb24
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-15 15:16:51 +0900

    use t.references

--- db/migrate/20240402075355_create_mentions.rb
+++ db/migrate/20240402075355_create_mentions.rb
@@ -1,13 +1,11 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.integer :source_id, null: false, index: true
-      t.integer :destination_id, null: false, index: true
+      t.references :source, null: false, foreign_key: { to_table: :reports }
+      t.references :destination, null: false, foreign_key: { to_table: :reports }
 
       t.timestamps
     end
-    add_foreign_key :mentions, :reports, column: :source_id
-    add_foreign_key :mentions, :reports, column: :destination_id
     add_index :mentions, %i[source_id destination_id], unique: true
   end
 end
