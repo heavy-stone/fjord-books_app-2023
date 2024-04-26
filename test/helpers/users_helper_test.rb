ec334eec32e45eedd482ce11a0dfb6f36d50e6d0
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-04-26 20:00:04 +0900

    use create instead of build

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -4,10 +4,9 @@ require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
   test 'should return user name or email' do
-    alice = build(:user, name: 'Alice Doe', email: '')
+    alice = create(:user, name: 'Alice Doe', email: 'alice@example.com')
     assert_equal 'Alice Doe', current_user_name(alice)
     alice.name = ''
-    alice.email = 'alice@example.com'
     assert_equal 'alice@example.com', current_user_name(alice)
   end
 end
